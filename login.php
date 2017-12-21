0b328325b1201c72685f74c002142f0b9bb2d67b
Caparas <zoelcaparas@gmail.com>
2017-12-21 19:21:06 +0100

    better login

--- login.php
+++ login.php
@@ -1,14 +1,43 @@
-<?php
-     include('includes/header.html');
+ <?php include("includes/header.html");
+     
+     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+ 
+ 	require ('requires/login_functions.php');
+ 	require ('requires/mysqli_connect.php');
+ 		
+ 	list ($check, $data) = check_login($dbc, $_POST['username'], $_POST['password']);
+ 	
+ 	if ($check)
+         {	
+             session_start();
+             $_SESSION['user_id'] = $data['UserID'];
+             $_SESSION['username'] = $data['Username'];
+ 
+             $_SESSION['agent'] = md5($_SERVER['HTTP_USER_AGENT']);
+ 
+             redirect_user('Game.php');		
+ 	}
+         else
+         {
+             $errors = $data;
+ 	}
+ 		
+ 	mysqli_close($dbc); // Close the database connection.
+ }
  ?>
- 
- <div class="">
-     <form action="">
-         
-     </form>
- </div>
- 
- 
- </div>
-     </body>
- </html>
+ <div class="panel-login-register">
+     <form action="Login.php" method="POST">
+        Username<br>
+        <input type="text" name="username" placeholder="Type your Username"/>
+        <br>
+        Password<br>
+        <input type="password" name="password" placeholder="Type your Password"/>
+        <br>
+        <input type="submit" value="GO!" />
+      </form>
+     <p><a href="Register.php">Create New Account</a></p>
+  </div>
+
+  <?php
+     include("includes/footer.html");
+ ?>
