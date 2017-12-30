fa750f244411b2723be111be7797a941d13bc646
Caparas <zoelcaparas@gmail.com>
2017-12-30 23:51:51 +0100

    Logout +Modi login

--- requires/login_functions.php
+++ requires/login_functions.php
@@ -49,4 +49,8 @@ function check_login($dbc, $username = '', $pass = '')
 
     return array(false, $errors);
 }
+function isLoggedIn()
+{
+    return ((isset($_SESSION['user_id']) && isset($_SESSION['username']) && isset($_SESSION['agent'])) && $_SESSION['agent'] === md5($_SERVER['HTTP_USER_AGENT']));
+}
  ?>
